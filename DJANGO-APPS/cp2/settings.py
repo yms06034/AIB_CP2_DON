cf0d3f2d0748494d3a7946d85f05bee9f16ec42f
Kim Sung Jin <yms06034@gmail.com>
2023-01-19 20:18:58 +0900

    Doing django setting

--- DJANGO-APPS/cp2/settings.py
+++ DJANGO-APPS/cp2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cp2_user.apps.Cp2UserConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
