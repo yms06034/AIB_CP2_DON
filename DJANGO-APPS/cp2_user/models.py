f83440029c24ea3eca0e022af1bb669ecb424ebe
Kim Sung Jin <yms06034@gmail.com>
2023-01-19 20:20:09 +0900

    Doing django setting

--- DJANGO-APPS/cp2_user/models.py
+++ DJANGO-APPS/cp2_user/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User as U
 # Create your models here.
 
 """ Test splite3 DB migrate """
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    nickname = models.CharField(max_length=100)
-    update_at = models.DateTimeField(auto_now=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+# class User(models.Model):
+#     name = models.CharField(max_length=20)
+#     nickname = models.CharField(max_length=100)
+#     update_at = models.DateTimeField(auto_now=True)
+#     create_at = models.DateTimeField(auto_now_add=True)
 
-class UserDetail(models.Model):
-    user = models.OneToOneField(U, on_delete=models.CASCADE)
-    ture_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+# class UserDetail(models.Model):
+#     user = models.OneToOneField(U, on_delete=models.CASCADE)
+#     ture_user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
